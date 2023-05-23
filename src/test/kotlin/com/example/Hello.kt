dbaa8e519eefb1313d83b3a4e459eb65905d9d12
jinwatanabe <jin.watanabe.6g@gmail.com>
2023-05-24 08:30:14 +0900

    TODO: unnecessary .txt file output

--- src/test/kotlin/com/example/Hello.kt
+++ src/test/kotlin/com/example/Hello.kt
@@ -1,10 +1,19 @@
 package com.example
 
 import com.thoughtworks.gauge.Step
+import com.codeborne.selenide.Condition.*
+import com.codeborne.selenide.Selenide.*
 
 class Hello {
     @Step("HelloWorldを出力する")
     fun displayHello(): String {
         return "HelloWorld"
     }
+
+    @Step("Googleのトップページを表示する")
+    fun test() {
+        open("https://www.google.com")
+        `$`(".MV3Tnb").shouldBe(exactText("Googleについて"))
+    }
+
 }
